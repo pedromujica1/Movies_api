b096f0b3a5307e78b7774ab4406a9752ece9f60c
pedromujica1 <miottopedro@gmail.com>
2023-08-20 13:50:51 -0300

    base html css

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-// New
+//Importando OpenAI package
 import OpenAI from 'openai';
 
 import dotenv from 'dotenv'
 
 dotenv.config()
 
+//Configurando chave da API
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY 
 });
 
+//Função para gerar imagens
+async function gerarImagens (prompt,tamanhoImagem) {
+	const imageGenaration = await openai.images.generate(
+	{
+		prompt: prompt,
+		n: 1,
+		size: tamanhoImagem
+	
+	}).then((data) => {
+		console.log(data);
+		return data;
+	});
+	
+}
 
-const prompt = "A sketch of a cat playing basketball";
-const numberOfImages = 1;
-const imageSize = "1024x1024";
-
-const imageGenaration = await openai.images.generate(	
-{
-	prompt: prompt,
-	n: numberOfImages,
-	size: imageSize
-
-}).then((data) => {
-	console.log(data);
-});
+//Exemplo de chamada de função
+gerarImagens("o senhor dos aneis","1024x1024");
+	
