1d599be0245c63a6ae63e3efa130469f0fc64a29
pedromujica1 <miottopedro@gmail.com>
2023-08-20 18:58:59 -0300

    funções feitas

--- nasa_api.js
+++ nasa_api.js
@@ -1,22 +1,47 @@
+//IMPORTANDO CHAVE API DE OUTRO ARQUIVO
+import { key } from "./config.js";
 
 //criar referências ao elemento da pagina
 const frm = document.querySelector("form");
 const data = document.getElementById("inData")
-//Importando modulo com a chave da api
-const config = require('./config.js')
+const resp1 = document.querySelector("h3");
 
 
-function gerarFoto(url) {
-    const date = new Date();
+//Função para verificar se data é válida
+function VerificarData(data) {
+    const data_atual = new Date();
+    let data_inserida = new Date(data)
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+    if (data_inserida>data_atual){
+        resp1.innerText=`Você ainda não pode viajar para o futuro! hahaha`;
+        return false
+    }
+    return true
+}
 
-    // This arrangement can be altered based on how we want the date's format to appear.
-    let currentDate = `${day}-${month}-${year}`;
-    console.log(currentDate); // "17-6-2022"
+async function gerarFoto(url) {
+    //Transformando dados do request em JSON
+    const dados = await fetch(url);
+        
+    //body do request
+    console.log(dados)
+
+    const dados_json = await dados.json();
+    //body do request em json
+    console.log(dados_json)
     
+    //Verificando se request foi realizado com sucesso
+    if(dados_json.status == 200 && dados_json.readyState == 4){
+        preencherDados(dados_json)
+    }
+    else{resp1.innerText=`Algo deu errado no request. Tente novamente!`;}
+}
+
+function preencherDados(response_data){
+    document.getElementById("title").textContent = response_data.title;
+    document.getElementById("date").textContent = response_data.date;
+    document.getElementById("pic").src = response_data.hdurl;
+    document.getElementById("explanation").textContent = response_data.explanation;
 }
 
 frm.addEventListener("submit",(e) => {
@@ -24,19 +49,17 @@ frm.addEventListener("submit",(e) => {
     e.preventDefault();
 
     const data_str = String(data.value);
-    const key = config.NASA_KEY
+    const api_key = key()
     
     //String da data
     console.log(data_str)
 
     //URL da requisição
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data_str}`;
-
-    gerarFoto(url)
-
-    
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${data_str}`;
 
 
-
-
+    if (VerificarData(data_str) == true) {
+        gerarFoto(url)
+    }
+    else{console.log("Data errada")}
 });
